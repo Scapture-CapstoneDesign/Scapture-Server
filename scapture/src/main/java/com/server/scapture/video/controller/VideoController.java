384a855d90eddcda0249994f80ca5c768f287aa9
이현승 <77336664+gesal03@users.noreply.github.com>
2024-05-26 23:59:45 +0900

    🗃️ Add S3 setting

---
+++ scapture/src/main/java/com/server/scapture/video/controller/VideoController.java
@@ -0,0 +1,12 @@
+package com.server.scapture.video.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/video")
+@RestController
+@RequiredArgsConstructor
+public class VideoController {
+
+}
