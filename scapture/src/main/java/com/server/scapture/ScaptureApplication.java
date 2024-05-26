31cfc9c2c95dde08891f9ad1a7fd754cb4e7b6e9
이현승 <77336664+gesal03@users.noreply.github.com>
2024-05-26 23:20:12 +0900

    ✨ Start project

---
+++ scapture/src/main/java/com/server/scapture/ScaptureApplication.java
@@ -0,0 +1,13 @@
+package com.server.scapture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaptureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaptureApplication.class, args);
+	}
+
+}
